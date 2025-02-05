9c784ed5e13e515e3890baf1dcd839900af58179
Youssef Bouali <53417796+youssefbouali@users.noreply.github.com>
2025-02-05 17:27:40 +0100

    Up Lagrangian Augmented

--- 7-Lagrangian_Augmented.py
+++ 7-Lagrangian_Augmented.py
@@ -76,7 +76,7 @@ def lagrangian_augmented(x0, λ0, c0, epsilon, max_iter):
     return x, λ
 
 # Example initialization
-x0 = np.array([0.5, 0.5])  # Initial guess for the decision variables
+x0 = np.array([0.1, 0.1])  # Initial guess for the decision variables
 λ0 = np.array([0.0, 0.0])  # Initial Lagrange multipliers
 c0 = 1.0  # Initial penalty parameter
 epsilon = 1e-6  # Convergence tolerance
